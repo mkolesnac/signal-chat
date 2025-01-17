d3eba1bd68769e71750a1628b49a070eab1a1f0e
Martin Kolesnac <kolesnac@ekosoft.sk>
2025-01-18 00:18:13 +0100

    Added APIClient tests

--- internal/client/apiclient.go
+++ internal/client/apiclient.go
@@ -11,20 +11,26 @@ import (
 
 type APIClient struct {
 	ServerURL     string
-	Authorization string
-	HTTPClient    *http.Client
+	authorization string
+	httpClient    *http.Client
 }
 
 func NewAPIClient(serverURL string) *APIClient {
 	return &APIClient{
 		ServerURL:  serverURL,
-		HTTPClient: &http.Client{},
+		httpClient: &http.Client{},
 	}
 }
 
 func (a *APIClient) UseBasicAuth(username, password string) {
+	if username == "" {
+		panic("username cannot be empty")
+	}
+	if password == "" {
+		panic("password cannot be empty")
+	}
 	credentials := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
-	a.Authorization = "Basic " + credentials
+	a.authorization = "Basic " + credentials
 }
 
 func (a *APIClient) Get(route string) ([]byte, error) {
@@ -51,11 +57,11 @@ func (a *APIClient) Post(route string, payload interface{}) ([]byte, error) {
 }
 
 func (a *APIClient) send(req *http.Request) ([]byte, error) {
-	if a.Authorization != "" {
-		req.Header.Set("Authorization", a.Authorization)
+	if a.authorization != "" {
+		req.Header.Set("Authorization", a.authorization)
 	}
 
-	resp, err := a.HTTPClient.Do(req)
+	resp, err := a.httpClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("error sending request: %s", err)
 	}
