96bf7db75d71f2a59cf9a2de36cd506ca97e406d
Martin Kolesnac <kolesnac@ekosoft.sk>
2025-02-01 15:30:30 +0100

    Removed not used code

--- client_web/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-import { withPigment } from '@pigment-css/nextjs-plugin';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default withPigment(nextConfig);
